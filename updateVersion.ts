a77894ed28c03bb99c9e3041836a165fed04dcb8
Jacob Lamb <jacobtlamb@hey.com>
2024-03-11 12:29:34 -0700

    Update deps && format astro w/ biome

--- updateVersion.ts
+++ updateVersion.ts
@@ -1,14 +1,15 @@
-import fse from "fs-extra/esm";
+import { hfs } from "@humanfs/node";
 async function updateVersion() {
 	const path = "./package.json";
-	const content = await fse.readJson(path);
+	const content = await hfs.json(path);
 	const current_date = new Date().toISOString().slice(0, 10).replace(/-/g, ".");
 	const newVersion = content.version.replace(
 		/(\d{4}\.\d{2}\.\d{2})/,
 		current_date,
 	);
 	content.version = newVersion;
-	await fse.writeJson(path, content, { spaces: 2 });
+	await hfs.write(path, JSON.stringify(content, null, 2));
+	console.log(`Updated version to ${newVersion}`);
 }
 
 updateVersion();
